81530b4e2b15897328df89d1fbb52adcd24cea36
sreeja-paluri <sreejapaluri07@gmail.com>
2026-03-23 20:02:35 +0530

    chore: update jwtUtils

--- backend/user-service/src/main/java/com/ripple/user_service/security/JwtUtil.java
+++ backend/user-service/src/main/java/com/ripple/user_service/security/JwtUtil.java
@@ -3,32 +3,41 @@ package com.ripple.user_service.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "mysupersecurejwtsecretkeymysupersecurejwtsecretkey123";
+    private final SecretKey signingKey;
+    private final long expiration;
+
+    public JwtUtil(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") long expiration
+    ) {
+        this.signingKey = Keys.hmacShaKeyFor(secret.getBytes());
+        this.expiration = expiration;
+    }
 
     public String generateToken(String email) {
-
         return Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(signingKey)
                 .compact();
     }
 
     public String extractEmail(String token) {
-
         return Jwts.parser()
-                .setSigningKey(SECRET_KEY.getBytes())
+                .verifyWith(signingKey)
                 .build()
-                .parseClaimsJws(token)
-                .getBody()
+                .parseSignedClaims(token)
+                .getPayload()
                 .getSubject();
     }
 }
