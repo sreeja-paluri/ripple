4ce7ebf96c5276893beeac21b01f402b91b5a05c
sreeja-paluri <sreejapaluri07@gmail.com>
2026-03-25 19:54:47 +0530

    feat: add friendcontroller and api response

---
+++ backend/friend-service/src/main/java/com/ripple/friend_service/controller/FriendController.java
@@ -0,0 +1,45 @@
+package com.ripple.friend_service.controller;
+
+import com.ripple.friend_service.dto.FriendRequestDTO;
+import com.ripple.friend_service.dto.FriendResponseDTO;
+import com.ripple.friend_service.response.ApiResponse;
+import com.ripple.friend_service.service.FriendService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/friends")
+@RequiredArgsConstructor
+public class FriendController {
+
+    private final FriendService service;
+
+    @PostMapping("/follow")
+    public ResponseEntity<ApiResponse<FriendResponseDTO>> follow(@RequestBody FriendRequestDTO request, @RequestHeader("X-User-Id") Long followerId){
+        FriendResponseDTO response = service.follow(followerId,request.getFollowingId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success("Started following", response));
+    }
+
+    @DeleteMapping("/unfollow/{followingId}")
+    public ResponseEntity<ApiResponse<Void>> unfollow(@PathVariable Long followingId, @RequestHeader("X-User-Id") Long followerId){
+        service.unfollow(followerId,followingId);
+        return ResponseEntity.ok(ApiResponse.success("Unfollowed Successfully", null));
+    }
+
+    @GetMapping ("/followers/{userId}")
+    public ResponseEntity<ApiResponse<List<FriendResponseDTO>>> getAllFollowers(@PathVariable Long userId){
+        List<FriendResponseDTO> followers = service.getAllFollowers(userId);
+        return ResponseEntity.ok().body(ApiResponse.success("All followers list", followers));
+    }
+
+    @GetMapping ("/following/{userId}")
+    public ResponseEntity<ApiResponse<List<FriendResponseDTO>>> getAllFollowing(@PathVariable Long userId){
+        List<FriendResponseDTO> following = service.getAllFollowing(userId);
+        return ResponseEntity.ok().body(ApiResponse.success("All following list", following));
+    }
+
+}
---
+++ backend/friend-service/src/main/java/com/ripple/friend_service/response/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.ripple.friend_service.response;
+
+import lombok.Data;
+
+@Data
+public class ApiResponse <T>{
+        private boolean success;
+        private String message;
+        private T data;
+
+
+        public ApiResponse(boolean success, String message, T data){
+            this.success = success;
+            this.message = message;
+            this.data = data;
+        }
+
+        public static <T> ApiResponse success(String message, T data){
+            return new ApiResponse(true, message, data);
+        }
+        public static <T> ApiResponse error(String message) {
+            return new ApiResponse(false, message, null);
+        }
+}
