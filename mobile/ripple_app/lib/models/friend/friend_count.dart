6ba590b7b9b4416d642a969b5349a4f33556201f
sreeja-paluri <sreejapaluri07@gmail.com>
2026-03-28 15:46:23 +0530

    feat: update FriendCount model with isFollowing field

--- mobile/ripple_app/lib/models/friend/friend_count.dart
+++ mobile/ripple_app/lib/models/friend/friend_count.dart
@@ -1,16 +1,25 @@
 class FriendCount {
   final int followingCount;
   final int followerCount;
+  final bool isFollowing;
 
-  FriendCount({required this.followingCount, required this.followerCount});
+  FriendCount(
+      {required this.followingCount,
+      required this.followerCount,
+      required this.isFollowing});
 
   factory FriendCount.fromJson(Map<String, dynamic> json) {
     return FriendCount(
         followingCount: json['followingCount'],
-        followerCount: json['followerCount']);
+        followerCount: json['followerCount'],
+        isFollowing: json['isFollowing']);
   }
 
   Map<String, dynamic> toJson() {
-    return {'followingCount': followingCount, 'followerCount': followerCount};
+    return {
+      'followingCount': followingCount,
+      'followerCount': followerCount,
+      'isFollowing': isFollowing
+    };
   }
 }
